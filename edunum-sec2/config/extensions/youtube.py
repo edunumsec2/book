566a4001cacc96e8b1cd1096aff045b120e3414d
Romain Edelmann <romain.edelmann@gmail.com>
2021-01-26 10:41:45 +0100

    Add directive for youtuve videos

---
+++ edunum-sec2/config/extensions/youtube.py
@@ -0,0 +1,63 @@
+
+import os
+import sys
+
+from docutils import nodes
+from docutils.parsers.rst import directives
+from sphinx.util.docutils import SphinxDirective
+
+class youtube_video(nodes.Element, nodes.General):
+    pass
+
+def visit_youtube_html(self, node):
+    extras = []
+    if node["start"] is not None:
+      extras.append(("start", str(node["start"])))
+    if node["controls"] is not None and not node["controls"]:
+      extras.append(("controls", "0"))
+
+    extra = ""
+    if len(extras) > 0:
+      extra = "?" + '&'.join(key + "=" + value for (key, value) in extras)
+
+    tag = self.starttag(node, "div", CLASS="youtube")
+    self.body.append(tag.strip())
+    self.body.append(
+      '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/' + 
+      node["vid"] + extra + '" frameborder="0" ' +
+      'allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ' + 
+      'allowfullscreen>"')
+
+def depart_youtube_html(self, node):
+    self.body.append("</iframe>")
+    self.body.append("</div>")
+
+
+class YouTubeVideo(SphinxDirective):
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+    option_spec = {
+      "start": directives.nonnegative_int,
+      "nocontrols": directives.flag
+    }
+    has_content = False
+
+    def run(self):
+        video_id = self.arguments[0]
+        container = youtube_video("",
+          vid=video_id,
+          start=self.options.get("start"),
+          controls="nocontrols" not in self.options)
+        self.set_source_info(container)
+
+        return [container]
+
+def setup(app):
+    app.add_directive("youtube", YouTubeVideo)
+    app.add_node(youtube_video, html=(visit_youtube_html, depart_youtube_html))
+
+    static_dir = os.path.join(os.path.dirname(__file__), "static")
+    app.connect("builder-inited", (lambda app: app.config.html_static_path.append(static_dir)))
+    app.add_js_file("youtube.js")
+    app.add_css_file("youtube.css")
--- edunum-sec2/content/demostests/edito/extensions.md
+++ edunum-sec2/content/demostests/edito/extensions.md
@@ -102,3 +102,63 @@ Dans le texte suivant, certains {bl}`>mots|trucs|machins` sont laissés {bl}`pou
 Dans le texte suivant, certains {bl}`>mots|trucs|machins` sont laissés {bl}`pour compte|>blancs|verts|seuls`.
 ```
 `````
+
+## Vidéos YouTube
+
+Pour ajouter une vidéo YouTube directement dans le document, utiliser la directive `youtube`.
+La directive attend en argument l'identifiant de la vidéo (généralement 11 caractères au format `base64url`).
+L'identifiant d'une vidéo YouTube peut être trouvé dans l'URL de la video, juste après la chaîne `?v=`.
+Par exemple, l'identifiant de la vidéo à l'URL `https://www.youtube.com/watch?v=dQw4w9WgXcQ` est `dQw4w9WgXcQ`.
+
+`````{tabbed} Aperçu
+```{youtube} uHKfrz65KSU
+```
+`````
+
+`````{tabbed} Code
+````{code-block} markdown
+```{youtube} uHKfrz65KSU
+```
+````
+`````
+
+### Options
+
+#### Départ de la lecture
+
+Par défaut, la lecture commence au début de la vidéo.
+Via l'option `:start:`, il est possible de faire commencer la lecture à un endroit différent de la vidéo.
+L'option `:start:` attend en argument un nombre entier qui indique la seconde à laquelle la lecture doit commencer.
+
+`````{tabbed} Aperçu
+```{youtube} _OBlgSz8sSM
+:start: 18
+```
+`````
+
+`````{tabbed} Code
+````{code-block} markdown
+```{youtube} _OBlgSz8sSM
+:start: 18
+```
+````
+`````
+
+#### Contrôles
+
+Par défaut, les boutons de contrôle de la vidéo sont affichés dans une barre en bas de la vidéo.
+Ils peuvent cependant être cachés via l'option `:nocontrols:`.
+
+`````{tabbed} Aperçu
+```{youtube} wGGqWwVb3sU
+:nocontrols:
+```
+`````
+
+`````{tabbed} Code
+````{code-block} markdown
+```{youtube} wGGqWwVb3sU
+:nocontrols:
+```
+````
+`````
