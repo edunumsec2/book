356f890fc07215f0ccccb842fce3fd13d528a882
Romain Edelmann <romain.edelmann@gmail.com>
2021-05-26 15:42:29 +0200

    Complete glossary prototype

--- config/extensions/glossary.py
+++ config/extensions/glossary.py
@@ -1,9 +1,9 @@
 from base64 import b64encode
 import csv
 from docutils import nodes
-from docutils.parsers.rst.states import SubstitutionDef
 from docutils.statemachine import ViewList
 from sphinx.errors import ExtensionError
+from sphinx.environment.adapters.toctree import TocTree
 from sphinx.util.docutils import SphinxDirective, SphinxRole
 from sphinx.util.nodes import nested_parse_with_titles
 from myst_parser.main import to_html
@@ -12,34 +12,101 @@ import os.path
 class glossary_list(nodes.General, nodes.Element):
     pass
 
-class glossary_definition(nodes.Inline, nodes.TextElement):
+class glossary_reference(nodes.Inline, nodes.TextElement):
     pass
 
-def visit_glossary_definition(self, node):
-    self.body.append('<span class="glossary-ref" ' + 
-        'data-definition="' + b64encode(to_html(node["definition"]).encode("UTF-8")).decode("UTF-8") + '" ' +
-        'data-number="' + node["number"] + '" ' +
-        'data-entry-name="' + b64encode(node["entry_name"].encode("UTF-8")).decode("UTF-8") + '">')
+def b64(string):
+    return b64encode(string.encode("UTF-8")).decode("UTF8")
 
-def depart_glossary_definition(self, node):
+def visit_glossary_reference(self, node):
+    self.body.append('<span class="glossary-ref" ' + 
+        'data-definition="' + b64(to_html(node["definition"])) + '" ' +
+        'data-number="' + node["number"] + '" ' +
+        ('data-glossary-uri="' + node["uri"] + '" ' if node["uri"] is not None else '') +
+        'data-entry-name="' + b64(node["entry_name"]) + '">')
+
+def depart_glossary_reference(self, node):
     self.body.append('</span>')
+
+def _glossary_file_name(app):
+    return os.path.normpath(os.path.join(
+        app.srcdir, '../glossaire.csv'))
+
+def _def_target(key, number):
+    return 'gl-def-%s-%s' % (key, number)
+
+def _parse_id(id):
+    key = id
+    number = '1'
+    if '@' in key:
+        [key, number] = key.split('@', 2)
+    return key, number
+
+def _parse_role_content(string):
+    key = string
+    display = string
+    if '|' in key:
+        [key, display] = key.split('|', 2)
+    key, number = _parse_id(key)
+    return key, number, display
+
+class GlossaryDefDirective(SphinxDirective):
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec = {}
+    has_content = False
+
+    def run(self):
+        env = self.env
+        env.note_dependency(_glossary_file_name(env.app))
+        key, number = _parse_id(self.arguments[0])
+
+        i = self.env.glossary_entries_index[key][number]
+        entry = self.env.glossary_entries[i]
+
+        adm_node = nodes.admonition('')
+        adm_node += nodes.title(entry['name'], entry['name'])
+
+        target_id = 'gl-%s-%d' % (key, env.new_serialno('gl-' + key))
+        adm_node += nodes.target('', '', ids=[target_id])
+
+        view_list = ViewList()
+        for line in entry['definition'].splitlines():
+            view_list.append(line, "glossaire.csv", 0)
+        nested_parse_with_titles(self.state, view_list, adm_node)
+
+        if not hasattr(env, 'glossary_references'):
+            env.glossary_references = {}
+        if not i in env.glossary_references:
+            env.glossary_references[i] = []
+        env.glossary_references[i].append({
+            'type': 'DEF',
+            'docname': env.docname,
+            'lineno': self.lineno,
+            'target_id': target_id,
+        })
+
+        return [adm_node]
 
 class GlossaryListDirective(SphinxDirective):
 
     def run(self):
+        self.env.note_reread()
         result_nodes = []
 
         for entry in self.env.glossary_entries:
             container = nodes.container(is_div=True, classes=["glossary-entry"])
 
-            target_id = 'gl-def-%s-%s' % (entry['key'], entry['number'])
+            target_id = _def_target(entry['key'], entry['number'])
             container += nodes.target('', '', ids=[target_id])
 
-            container += nodes.strong(entry['name'], entry['name'], classes=["glossary-name"])
+            container += nodes.strong(entry['name'], entry['name'],
+                                      classes=["glossary-name"])
 
             view_list = ViewList()
             for line in entry['definition'].splitlines():
-                view_list.append(line, "glossary.csv", 0)
+                view_list.append(line, "glossaire.csv", 0)
             temp_node = nodes.section()
             temp_node.document = self.state.document
 
@@ -56,7 +123,7 @@ class GlossaryListDirective(SphinxDirective):
 
 def load_glossary_csv(app, env, docnames):
     entries = []
-    with open(os.path.normpath(os.path.join(app.srcdir, '../glossaire.csv')), newline='') as csv_file:
+    with open(_glossary_file_name(app), newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
         for i, line in enumerate(csv_reader):
             entries.append({
@@ -79,17 +146,58 @@ def load_glossary_csv(app, env, docnames):
     env.glossary_entries = entries
     env.glossary_entries_index = index
 
+def index_link_node(app, fromdocname, refs):
+    toc = TocTree(app.env)
+    par_nodes = []
+    labels = {
+        'DEF': 'Définitions:',
+        'USE': 'Usages:',
+    }
+    for type in ['DEF', 'USE']:
+        relevant_refs = [ref for ref in refs if ref['type'] == type]
+        if not relevant_refs:
+            continue
+        par_node = nodes.paragraph(classes=["glossary-refs"])
+        par_node += nodes.strong(labels[type], labels[type])
+        
+        for ref in refs:
+            if ref['type'] != type:
+                continue
+            ref_node = nodes.reference('', '')
+            docnames = toc.get_toctree_ancestors(ref['docname'])
+            if not docnames:
+                docnames = [ref['docname']]
+            for i, docname in enumerate(reversed(docnames)):
+                if i < len(docnames) - 1:
+                    for child in app.env.titles[docname].children:
+                        ref_node.append(child)
+                    ref_node.append(nodes.Text('/', '/'))
+                else:
+                    strong_node = nodes.strong('', '')
+                    for child in app.env.titles[docname].children:
+                        strong_node.append(child)
+                    ref_node.append(strong_node)
+            ref_node['refdocname'] = ref['docname']
+            ref_node['refuri'] = app.builder.get_relative_uri(
+                fromdocname, ref['docname'])
+            ref_node['refuri'] += "#" + ref['target_id']
+            par_node += ref_node
+        par_nodes.append(par_node)
+    return par_nodes
+
 def process_glossary_list(app, doctree, fromdocname):
     env = app.builder.env
 
     for node in doctree.traverse(glossary_list):
         index = node.glossary_index
-        node.replace_self([nodes.Text(str(ref)) for ref in env.glossary_references.get(index, [])])
+        refs = env.glossary_references.get(index, [])
+        node.replace_self(index_link_node(
+            app, fromdocname, refs))
 
 def purge_glossary_references(app, env, docname):
     if not hasattr(env, 'glossary_references'):
         return
-    
+
     for i, refs in env.glossary_references.items():
          env.glossary_references[i] = [ref for ref in refs
                                        if ref['docname'] != docname]
@@ -107,22 +215,26 @@ def merge_glossary_references(app, env, docnames, other):
 class GlossaryRole(SphinxRole):
     def run(self):
         env = self.env
-        key = self.text
-        number = '1'
-        display = self.text
-        if '|' in key:
-            [key, display] = key.split('|', 2)
-        if '@' in key:
-            [key, number] = key.split('@', 2)
+        env.note_dependency(_glossary_file_name(env.app))
+
+        key, number, display = _parse_role_content(self.text)
         i = env.glossary_entries_index[key][number]
         entry = env.glossary_entries[i]
+
 
         target_id = 'gl-%s-%d' % (key, env.new_serialno('gl-' + key))
         target_node = nodes.target('', '', ids=[target_id])
 
-        node = glossary_definition(display, display,
+        glossary_uri = env.app.config.glossary_doc
+        if glossary_uri is not None:
+            glossary_uri = env.app.builder.get_relative_uri(
+                env.docname, glossary_uri)
+            glossary_uri += '#' + _def_target(key, number)
+
+        node = glossary_reference(display, display,
             definition=entry['definition'],
             entry_name=entry['name'],
+            uri=glossary_uri,
             number=entry['number'],
             classes=["glossary-ref"], **self.options)
 
@@ -131,6 +243,7 @@ class GlossaryRole(SphinxRole):
         if not i in env.glossary_references:
             env.glossary_references[i] = []
         env.glossary_references[i].append({
+            'type': 'USE',
             'docname': env.docname,
             'lineno': self.lineno,
             'target_id': target_id,
@@ -140,13 +253,15 @@ class GlossaryRole(SphinxRole):
 
 def setup(app):
     app.add_node(glossary_list)
-    app.add_node(glossary_definition, html=(visit_glossary_definition, depart_glossary_definition))
+    app.add_node(glossary_reference,
+        html=(visit_glossary_reference, depart_glossary_reference))
 
     app.connect('env-before-read-docs', load_glossary_csv)
     app.connect('doctree-resolved', process_glossary_list)
     app.connect('env-purge-doc', purge_glossary_references)
     app.connect('env-merge-info', merge_glossary_references)
 
+    app.add_directive('definition', GlossaryDefDirective)
     app.add_directive('glossaire', GlossaryListDirective)
     app.add_role('glo', GlossaryRole())
 
--- config/extensions/static/glossary.js
+++ config/extensions/static/glossary.js
@@ -13,19 +13,30 @@ function references_ready() {
         '<div id="tooltip-container">' +
         '<h1 id="tooltip-name"></h1>' +
         '<div id="tooltip-description"></div>' +
+        '<div id="tooltip-link-div">' +
+        '<a id="tooltip-link" href="#">Voir dans le glossaire</a>' +
+        '</div>' +
         '<div id="tooltip-close"><div>&#215;</div></div>' +
         '</div>' +
         '<div id="tooltip-arrow" data-popper-arrow></div>';
     document.body.appendChild(tooltip);
     const tooltipName = document.getElementById("tooltip-name");
     const tooltipDesc = document.getElementById("tooltip-description");
+    const tooltipLink = document.getElementById("tooltip-link");
 
     var popperInstance = null;
     var activeElement = null;
 
     function createTooltip(elem) {
-        tooltipName.innerText = b64DecodeUnicode(elem.dataset.entryName)
-        tooltipDesc.innerHTML = b64DecodeUnicode(elem.dataset.definition)
+        tooltipName.innerText = b64DecodeUnicode(elem.dataset.entryName);
+        tooltipDesc.innerHTML = b64DecodeUnicode(elem.dataset.definition);
+        if ("glossaryUri" in elem.dataset) {
+            tooltipLink.style.display = 'inline';
+            tooltipLink.setAttribute("href", elem.dataset.glossaryUri);
+        }
+        else {
+            tooltipLink.style.display = 'none';
+        }
         tooltip.style.display = "block";
 
         popperInstance = Popper.createPopper(elem, tooltip, {
@@ -56,6 +67,10 @@ function references_ready() {
     }
 
     document.getElementById("tooltip-close").addEventListener("click", hideTooltip);
+    tooltipLink.addEventListener("click", function() {
+        hideTooltip();
+        return true;
+    });
 
     var refs = document.getElementsByClassName("glossary-ref");
     for (const ref of refs) {
