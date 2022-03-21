64cc105d432a10f22fa995dce2e66cb6fe02465e
Romain Edelmann <romain.edelmann@gmail.com>
2022-03-21 17:02:28 +0100

    Fix dark mode for codeplay and questions

--- src/static/scripts/frames.js
+++ src/static/scripts/frames.js
@@ -14,7 +14,25 @@ function populateFrame(self, initialise) {
   var frames = document.getElementsByClassName("codeframe")
   for (const frame of frames) {
     if (frame.contentWindow === self) {
+      frame.contentDocument.body.dataset.theme = document.body.dataset.theme;
       initialise(frame);
     }
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const buttons = document.getElementsByClassName("theme-toggle");
+  const frames = document.getElementsByClassName("codeframe");
+  
+  for (const button of buttons) {
+    button.addEventListener("click", function() {
+      const theme = document.body.dataset.theme;
+      console.log(theme);
+      for (const frame of frames) {
+        const innerDoc = frame.contentDocument;
+        innerDoc.body.dataset.theme = theme;
+        innerDoc.refreshTheme();
+      }
+    });
+  }
+});
