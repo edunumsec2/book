94465a753b5c731f88ba9017833448394ade3345
Jean-Philippe Pellet <jppellet@gmail.com>
2022-07-11 16:59:35 +0200

    First attempt at auto generation of PNGs for logic diagrams

--- src/exts/logic.py
+++ src/exts/logic.py
@@ -1,20 +1,99 @@
-from base64 import b64encode
+import asyncio
+from dataclasses import dataclass
+from functools import cached_property
+import base64
+import os
+import sys
+import hashlib
+from typing import Any, List, Tuple, Union
+import pyppeteer  # type: ignore
+from pyppeteer.page import Page  # type: ignore
 
-from docutils import nodes
-from docutils.parsers.rst import directives
-from sphinx.util.docutils import SphinxDirective, SphinxRole
-from myst_parser.main import to_html
+from sphinx.application import Sphinx
+from sphinx.util import logging
+from sphinx.util.docutils import SphinxDirective, SphinxRole, SphinxTranslator
+from docutils import nodes  # type: ignore
+from docutils.nodes import Node, system_message  # type: ignore
+from docutils.parsers.rst import directives  # type: ignore
+from myst_parser.main import MdParserConfig, to_html
 
 # import urllib
 # import zlib
 # import lzstring
 
+logger = logging.getLogger(__name__)
+
+StringOrList = Union[str, List[str]]
+
+
+def md5(string: str) -> str:
+    md5_builder = hashlib.md5()
+    md5_builder.update(string.encode("utf-8"))
+    return md5_builder.hexdigest()
+
+
+def await_blocking(func: Any) -> Any:
+    return asyncio.get_event_loop().run_until_complete(func)
+
+
+@dataclass
+class LogicDiagramData:
+    data_json: str
+    mode: str
+    height: int | None
+
+    @cached_property
+    def content_based_filename(self) -> str:
+        height_str = "-1" if self.height is None else str(self.height)
+        spec = "/".join([self.data_json, self.mode, height_str])
+        spec_md5 = md5(spec)  # should be 32 chars hex
+        return f"{spec_md5}.png"
+
+
+async def browser_launch() -> Page:
+    browser = await pyppeteer.launch()
+    page = await browser.newPage()
+    return page
+
+
+async def browser_close(page: Page) -> None:
+    await page.browser.close()
+
+
+async def to_png(data: LogicDiagramData, output_file: str, page: Page) -> bool:
+    await page.goto("https://logic.modulo-info.ch", waitUntil="networkidle2")
+    result = await page.evaluate(
+        f"""() => {{
+        const editor = window.Logic.singleton
+        if (typeof editor === 'undefined') {{
+            return "error: no editor"
+        }}
+
+        const dataJson = JSON.stringify({data.data_json})
+        editor.load(dataJson)
+        editor.setModeFromString("{data.mode}")
+        return editor.toPNGBase64({"undefined" if data.height is None else data.height})
+    }}"""
+    )
+
+    if result is None:
+        logger.error("error: no result from PNG conversion")
+        return False
+
+    if result.startswith("error:"):
+        logger.error(result)
+        return False
+
+    with open(output_file, "wb") as f:
+        f.write(base64.b64decode(result))
+    return True
+
+
 class logic_diagram(nodes.Element, nodes.General):
     pass
 
 
-def begin_logic_diagram_html(self, node):
-
+def begin_logic_diagram_html(self: SphinxTranslator, node: Node) -> None:
     content = node["content"].strip()
     ref = node["ref"]
     height = node["height"]
@@ -38,32 +117,55 @@ def begin_logic_diagram_html(self, node):
         attrs_str += ' showonly="' + showonly + '"'
 
     self.body.append(
-        "<logic-editor"
-        + attrs_str
-        + ">"
-        + "\n"
-        + '<script type="application/json">'
-        + "\n"
-        + content
-        + "\n"
+        "<logic-editor" + attrs_str + ">" + "\n" + '<script type="application/json">' + "\n" + content + "\n"
     )
 
 
-def end_logic_diagram_html(self, node):
+def end_logic_diagram_html(self: SphinxTranslator, node: Node) -> None:
     self.body.append("</script>")
     self.body.append("</logic-editor>")
     self.body.append("</div>")
 
 
-def begin_logic_diagram_latex(self, node):
-    self.body.append("\n \\fbox{logic diagram} \\\ ")
+def begin_logic_diagram_latex(self: SphinxTranslator, node: Node) -> None:
 
-def end_logic_diagram_latex(self, node):
-   self.body.append("\n")
+    content = node["content"].strip()
+    height = node["height"]
+    mode = node["mode"]
 
-    
+    png_folder = os.path.join(self.builder.outdir, "logic_png")
+    if not os.path.exists(png_folder):
+        os.makedirs(png_folder)
 
-def directive_mode(argument):
+    data = LogicDiagramData(content, mode, height)
+    target_file = os.path.join(png_folder, data.content_based_filename)
+
+    include_image = False
+    if os.path.exists(target_file):
+        include_image = True
+    else:
+        if browser_page is None:
+            logger.error("browser page is not initialized to generate PNGs for logic diagrams")
+        else:
+            include_image = await_blocking(to_png(data, target_file, browser_page))
+            if include_image:
+                logger.info("Generated logic diagram: %s", target_file)
+            else:
+                logger.error("Failed to generate logic diagram: %s", target_file)
+
+    if include_image:
+        self.body.append(
+            f"\n\\begin{{center}}\n  \\includegraphics[scale=0.33]{{{target_file}}}\n\\end{{center}}\n"
+        )
+    else:
+        self.body.append("\n \\fbox{logic diagram} \\\ ")
+
+
+def end_logic_diagram_latex(self: SphinxTranslator, node: Node) -> None:
+    self.body.append("\n")
+
+
+def directive_mode(argument: str) -> Any:
     return directives.choice(argument, ("static", "tryout", "connect", "design", "full"))
 
 
@@ -79,7 +181,7 @@ class LogicDiagram(SphinxDirective):
     }
     has_content = True
 
-    def run(self):
+    def run(self) -> List[Node]:
         self.assert_has_content()
 
         ref = self.options.get("ref", "")
@@ -105,35 +207,37 @@ class logic_highlight(nodes.Inline, nodes.TextElement):
     pass
 
 
-
-def _parse_highlight_content(string):
-    def _parse_ids(string):
+def _parse_highlight_content(string: str) -> Tuple[StringOrList, StringOrList, str]:
+    def _parse_ids(string: str) -> List[str]:
         if string.startswith("{") and string.endswith("}"):
             return string[1:-1].split(",")
         return [string]
 
-    if '|' in string:
-        [refs, display] = string.split('|', 2)
-        if '.' in refs:
-            [diagramrefs, componentrefs] = refs.split('.', 2)
+    if "|" in string:
+        [refs, display] = string.split("|", 2)
+        if "." in refs:
+            [diagramrefs, componentrefs] = refs.split(".", 2)
             return _parse_ids(diagramrefs), _parse_ids(componentrefs), display
 
     return "", "", string
 
 
 class LogicHighlightRefRole(SphinxRole):
-    def run(self):
+    def run(self) -> Tuple[List[Node], List[system_message]]:
         diagramrefs, componentrefs, display = _parse_highlight_content(self.text)
-        node = logic_highlight("", "",
+        myst_config = self.env.myst_config  # type: ignore
+        node = logic_highlight(
+            "",
+            "",
             diagramrefs=diagramrefs,
             componentrefs=componentrefs,
             display=display,
-            config=self.env.myst_config,
+            config=myst_config,
         )
         return [node], []
 
 
-def _render_inline(source: str, config) -> str:
+def _render_inline(source: str, config: MdParserConfig) -> str:
     # WARNING: this should not be called when creating the nodes
     # as it screws up the global parser state
     html = str(to_html(source, config=config)).strip()
@@ -141,14 +245,19 @@ def _render_inline(source: str, config) -> str:
         html = html[3:-4]
     return html
 
-def begin_logic_highlight_html(self, node):
-    def _to_js_string(refs):
+
+def begin_logic_highlight_html(self: SphinxTranslator, node: Node) -> None:
+    def _to_js_string(refs: List[str]) -> str:
         if len(refs) == 1:
             return '"' + refs[0] + '"'
-        return '[' + ', '.join(['"' + r + '"' for r in refs]) + ']'
+        return "[" + ", ".join(['"' + r + '"' for r in refs]) + "]"
 
     js_on_click = (
-        'Logic.highlight(' + _to_js_string(node["diagramrefs"]) + ', ' + _to_js_string(node["componentrefs"]) + ')'
+        "Logic.highlight("
+        + _to_js_string(node["diagramrefs"])
+        + ", "
+        + _to_js_string(node["componentrefs"])
+        + ")"
     )
     tag = self.starttag(
         node,
@@ -161,37 +270,41 @@ def begin_logic_highlight_html(self, node):
     self.body.append(content)
 
 
-
-def end_logic_highlight_html(self, node):
+def end_logic_highlight_html(self: SphinxTranslator, node: Node) -> None:
     self.body.append("</span>")
 
 
 def _render_latex(source: str) -> str:
     # ugly hack, should used child nodes instead (micha)
     source.strip()
-    source = source.replace('_','')
+    source = source.replace("_", "")
     return source
-    
-    
-def begin_logic_highlight_latex(self, node):
+
+
+def begin_logic_highlight_latex(self: SphinxTranslator, node: Node) -> None:
     content = _render_latex(node["display"])
     self.body.append(content)
 
-def end_logic_highlight_latex(self, node):
+
+def end_logic_highlight_latex(self: SphinxTranslator, node: Node) -> None:
     pass
+
 
 class logic_gate(nodes.Inline, nodes.TextElement):
     pass
 
+
 class LogicGateRole(SphinxRole):
-    def run(self):
-        node = logic_gate("", "",
+    def run(self) -> Tuple[List[Node], List[system_message]]:
+        node = logic_gate(
+            "",
+            "",
             display=self.text,
         )
         return [node], []
 
 
-def begin_logic_gate_html(self, node):
+def begin_logic_gate_html(self: SphinxTranslator, node: Node) -> None:
     tag = self.starttag(
         node,
         "span",
@@ -200,21 +313,44 @@ def begin_logic_gate_html(self, node):
     self.body.append(tag.strip())
     self.body.append(node["display"])
 
-def end_logic_gate_html(self, node):
+
+def end_logic_gate_html(self: SphinxTranslator, node: Node) -> None:
     self.body.append("</span>")
 
-def begin_logic_gate_latex(self, node):
-    self.body.append("\n \\fbox{logic gate} \\\ ")
 
-def end_logic_gate_latex(self, node):
-    pass
-    
-    
-def setup(app):
+def begin_logic_gate_latex(self: SphinxTranslator, node: Node) -> None:
+    self.body.append("\\textbf{")
+    self.body.append(node["display"])
+
+
+def end_logic_gate_latex(self: SphinxTranslator, node: Node) -> None:
+    self.body.append("}")
+
+
+browser_page: Union[Page, None] = None
+
+
+def build_starting(app: Sphinx) -> None:
+    global browser_page
+    if app.builder is not None and app.builder.name == "latex":
+        logger.info("starting headless browser")
+        browser_page = await_blocking(browser_launch())
+
+
+def build_finished(app: Sphinx, exception: Any) -> None:
+    global browser_page
+    if app.builder is not None and app.builder.name == "latex":
+        logger.info("shutting down headless browser")
+        await_blocking(browser_close(browser_page))
+        browser_page = None
+
+
+def setup(app: Sphinx) -> None:
     app.add_directive("logic", LogicDiagram)
-    app.add_node(logic_diagram,
-                 html=(begin_logic_diagram_html, end_logic_diagram_html),
-                 latex=(begin_logic_diagram_latex, end_logic_diagram_latex)
+    app.add_node(
+        logic_diagram,
+        html=(begin_logic_diagram_html, end_logic_diagram_html),
+        latex=(begin_logic_diagram_latex, end_logic_diagram_latex),
     )
     app.add_js_file("https://logic.modulo-info.ch/simulator/lib/bundle.js")
 
@@ -222,15 +358,17 @@ def setup(app):
     app.add_node(
         logic_highlight,
         html=(begin_logic_highlight_html, end_logic_highlight_html),
-        latex=(begin_logic_highlight_latex, end_logic_highlight_latex)
+        latex=(begin_logic_highlight_latex, end_logic_highlight_latex),
     )
 
     app.add_role("lg", LogicGateRole())
     app.add_node(
         logic_gate,
         html=(begin_logic_gate_html, end_logic_gate_html),
-        latex=(begin_logic_gate_latex, end_logic_gate_latex)
+        latex=(begin_logic_gate_latex, end_logic_gate_latex),
     )
 
-
     app.add_css_file("logic.css")
+
+    app.connect("builder-inited", build_starting)
+    app.connect("build-finished", build_finished)
