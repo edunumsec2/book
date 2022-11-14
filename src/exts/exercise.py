181e227ac056a9ad16b0d86f33ff55f3923f6c3e
Micha Hersch <12912659+mihersch@users.noreply.github.com>
2022-11-14 18:16:34 +0100

    ajout admonition exercise et togofurther

--- src/exts/exercise.py
+++ src/exts/exercise.py
@@ -24,9 +24,38 @@ class Exercise(SphinxDirective):
 
         return [admonition]
 
+
+class ToGoFurther(SphinxDirective):
+    required_arguments = 0
+    optional_arguments = 1
+    final_argument_whitespace = True
+    has_content = True
+
+    def run(self):
+        self.assert_has_content()
+
+        admonition = nodes.admonition("")
+
+        title ="Pour aller plus loin"
+        #if len(self.arguments) > 0: 
+        #    title += ": " + self.arguments[0]
+
+        textnodes, _ = self.state.inline_text(title, self.lineno)
+        label = nodes.title(title, *textnodes)
+        admonition += label
+
+        content = nodes.container("", is_div=True, classes=["togofurther-content"])
+        self.state.nested_parse(self.content, self.content_offset, content)
+        admonition += content
+
+        return [admonition]
+
+
+
+
 def setup(app):
     app.add_directive("exercise", Exercise)
-
+    app.add_directive("togofurther", ToGoFurther)
     # static_dir = os.path.join(os.path.dirname(__file__), "static")
     # app.connect("builder-inited", (lambda app: app.config.html_static_path.append(static_dir)))
     # app.add_js_file("exercise.js")
