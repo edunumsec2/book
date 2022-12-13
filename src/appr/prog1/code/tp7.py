fe1e692cf332e98bbfe9f106767a8ef66da4df14
Raphael Holzer <raphael.holzer@citycable.ch>
2022-12-13 23:08:52 +0100

    update définir/paramétrer

---
+++ src/appr/prog1/code/tp7.py
@@ -0,0 +1,34 @@
+"""
+tp7 - positionner
+
+Nom : 
+Classe :
+Date :
+
+- nommez ce fichier : tp7_classe_prenom (minuscules, sans accents)
+- créez un dessin libre avec des polygons (style points à relier)
+- utilisez des tuples avec 50+ points au total
+- vous pouvez utiliser 1 plusieurs polygones
+- placez les numéros sur l'un et des coordonnés sur un autre polygone
+- montrez au minimum 4 version différents (taille, mirroir, position, etc.)
+- déposez sur Moodle les 3 fichiers:
+    tp7_classe_prenom.py     (code)
+    tp7_classe_prenom.eps    (image vectorielle)
+    tp7_classe_preneom.jpg   (image)
+"""
+from turtle import *
+from tkinter import *
+
+d = 20
+
+
+
+# ajoutez vos fonctions ici
+
+pacman()
+
+
+
+# remplacer avec tp3_classe_prenom
+Screen().getcanvas().postscript(file='tp2.eps')
+done()
