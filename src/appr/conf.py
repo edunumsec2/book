dc600e073774c0dcc1ce58e01126d6f290ee7d82
Jean-Philippe Pellet <jppellet@gmail.com>
2023-04-26 10:58:41 +0200

    Tweak lengths, margins, etc.

--- src/appr/conf.py
+++ src/appr/conf.py
@@ -182,7 +182,7 @@ latex_elements = {
     'preamble': r'\input{../../../src/static/latex/customize} \fvset{fontsize=\small}',
     'pxunit': '0.5bp',
     'figure_align': 'H',
-    'maketitle':  r'\maketitle',
+    'maketitle':  r'\maketitle\cleardoublepage',
     # some command redefinitions only take effect after the first \tableofcontents, so we add this hook
     'tableofcontents': '\\sphinxtableofcontents\n\n\\input{../../../src/static/latex/customize_doc}\n',
     'pointsize': '11pt',
